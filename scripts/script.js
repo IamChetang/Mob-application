1c2499581e6951c0ac9255c4a7cd0b56993d556f
IamChetang <chetang568568@gmail.com>
2022-02-05 22:34:13 +0530

    completed final ui

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 // 'use strict';
-document.getElementById('toggle1').addEventListener('click', toggleClass);
-document.getElementById('toggle2').addEventListener('click', toggleClass);
-document.getElementById('toggle3').addEventListener('click', toggleClass);
-document.getElementById('toggle4').addEventListener('click', toggleClass);
+document.getElementById('toggle').addEventListener('click', toggleClass);
 
 function toggleClass() {
   let toggleAble = document.querySelector('.second-list');
