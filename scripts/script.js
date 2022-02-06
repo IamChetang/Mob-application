bd39450c0e343d361fa817ec6c0eb61d89a1cb96
IamChetang <chetang568568@gmail.com>
2022-02-06 17:27:44 +0530

    gu

--- scripts/script.js
+++ scripts/script.js
@@ -10,11 +10,10 @@ function toggleClass() {
 // var i;
 // for (i = 0; i < acc.length; i++) {
 //   acc[i].addEventListener('click', function () {
-//     /* Toggle between adding and removing the "active" class,
-//     to highlight the button that controls the panel */
+//
 //     this.classList.toggle('active');
 
-//     /* Toggle between hiding and showing the active panel */
+//
 //     var panel = this.nextElementSibling;
 //     if (panel.style.display === 'none') {
 //       panel.style.display = 'flex';
