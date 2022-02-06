5eab1ae31d530fd90e2bf20dc838f59eb51bd1ab
IamChetang <chetang568568@gmail.com>
2022-02-06 17:23:22 +0530

    checked accordion

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,21 @@ function toggleClass() {
   let toggleAble = document.querySelector('.second-list');
   toggleAble.classList.toggle('active');
 }
+
+// var acc = document.getElementsByClassName('list-section');
+// var i;
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener('click', function () {
+//     /* Toggle between adding and removing the "active" class,
+//     to highlight the button that controls the panel */
+//     this.classList.toggle('active');
+
+//     /* Toggle between hiding and showing the active panel */
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === 'none') {
+//       panel.style.display = 'flex';
+//     } else {
+//       panel.style.display = 'none';
+//     }
+//   });
+// }
